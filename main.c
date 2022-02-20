b28fdd91cb1c9a7e4366b71bad88c5c2f2120e59
CarlosCHMS <msouza.ch@gmail.com>
2022-02-20 15:14:06 -0300

    Initial files

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+#include<sys/time.h>
+#include<omp.h>
+#include"input.h"
+
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+
+}
