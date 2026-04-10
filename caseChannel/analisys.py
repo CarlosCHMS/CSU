4f7fe5e015f3252ead74d1e0689a4827d135cd95
carlosCHMS <msouza.ch@gmail.com>
2026-04-10 16:31:11 -0300

    improve channel case

--- caseChannel/analisys.py
+++ caseChannel/analisys.py
@@ -6,13 +6,15 @@ from su2MeshReader import reader
 import sys
 import numpy
  
+def csv2dict(fileName):
+    data = numpy.genfromtxt(fileName, delimiter=',', names=True, dtype=None, encoding=None)
+    
+    return {name: data[name] for name in data.dtype.names}
+
 class solution():
 
     def __init__(self, meshFile, solFile):
-
-        self.gamma = 1.4
-        self.Rgas = 287.5
-
+        
         self.mesh = reader(meshFile)
         
         self.x = self.mesh.x
@@ -23,35 +25,9 @@ class solution():
                 
         ff = open(solFile)
 
-        self.r = []
-        self.ru = []
-        self.rv = []
-        self.rE = []
-
-        first = True
-        for row in ff:
-            if first:
-                first = False
-            else:
-                aux = row.split(',')
-                self.r.append(float(aux[0]))
-                self.ru.append(float(aux[1]))
-                self.rv.append(float(aux[2]))
-                self.rE.append(float(aux[3]))
+        self.data = csv2dict(solFile)
 
         ff.close()
-
-        self._toArray()
-        self.calcPMT()
-    
-    def _toArray(self):
-
-        self.r = numpy.array(self.r)
-        self.ru = numpy.array(self.ru)
-        self.rv = numpy.array(self.rv)
-        self.rE = numpy.array(self.rE)
-
-        return None
         
     def elemToTri(self):
     
@@ -65,38 +41,6 @@ class solution():
             
         return None                
         
-    def calcPMT(self):
-       
-        Np = len(self.mesh.p)
-       
-        self.p = numpy.zeros(Np)
-        self.mach = numpy.zeros(Np)
-        self.entro = numpy.zeros(Np)               
-        self.H = numpy.zeros(Np)                
-       
-        for ii in range(0, Np):
-       
-            if(self.con[ii] > 0):
-       
-                u = self.ru[ii]/self.r[ii]
-                v = self.rv[ii]/self.r[ii]
-                E = self.rE[ii]/self.r[ii]
-                
-                RT = (E - (u**2 + v**2)/2)*(self.gamma - 1)
-                
-                self.p[ii] = RT*self.r[ii]
-                
-                c = numpy.sqrt(self.gamma*RT)
-                V = numpy.sqrt(u**2 + v**2)
-                
-                self.mach[ii] = V/c
-
-                self.entro[ii] = self.p[ii]/(self.r[ii]**self.gamma)
-
-                self.H[ii] = E + self.p[ii]/self.r[ii]
-        
-        return None        
-
     def pConnect(self):
     
         self.con = numpy.zeros(len(self.mesh.p))
@@ -154,13 +98,13 @@ if __name__=="__main__":
     
     path = sys.argv[1]
 
-    s = solution(path+"mesh.su2", path+"solution.csv")
+    s = solution(path+"mesh.su2", path+"solution2.csv")
 
     triang = mtri.Triangulation(s.x, s.y, s.elem)
 
     plt.figure()
     plt.title("Static pressure")
-    plt.tricontourf(triang, s.p)
+    plt.tricontourf(triang, s.data['p'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar()
@@ -169,7 +113,7 @@ if __name__=="__main__":
 
     plt.figure()
     plt.title("Mach")
-    plt.tricontourf(triang, s.mach)
+    plt.tricontourf(triang, s.data['mach'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar() 
@@ -178,7 +122,7 @@ if __name__=="__main__":
         
     plt.figure()
     plt.title("Entropy")    
-    plt.tricontourf(triang, s.entro)
+    plt.tricontourf(triang, s.data['s'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar() 
@@ -187,7 +131,7 @@ if __name__=="__main__":
 
     plt.figure()
     plt.title("Entalpy")
-    plt.tricontourf(triang, s.H)
+    plt.tricontourf(triang, s.data['H'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar() 
@@ -202,21 +146,21 @@ if __name__=="__main__":
     plt.semilogy(conv.varList[3]/conv.varList[3][0])    
     plt.semilogy(conv.varList[4]/conv.varList[4][0])       
     plt.grid(True)
-    plt.xlabel("iterations [100]")  
+    plt.xlabel("iterations [-]")  
     plt.ylabel("Residuos")            
     plt.show()
     
     plt.figure()
     plt.plot(conv.varList[5])
     plt.grid(True)
-    plt.xlabel("iterations [100]")  
+    plt.xlabel("iterations [-]")  
     plt.ylabel("Cx_p [-]")            
     plt.show()
 
     plt.figure()
     plt.plot(conv.varList[7])
     plt.grid(True)
-    plt.xlabel("iterations [100]")  
+    plt.xlabel("iterations [-]")  
     plt.ylabel("Cy_p [-]")            
     plt.show()
 
