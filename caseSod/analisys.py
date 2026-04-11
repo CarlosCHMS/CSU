fde1fd9526facaf45f79079e6b777a5f1448145a
carlosCHMS <msouza.ch@gmail.com>
2026-04-11 16:58:59 -0300

    improve the sod cases

--- caseSod/analisys.py
+++ caseSod/analisys.py
@@ -7,13 +7,15 @@ import sys
 import numpy
 import characteristics as ch
  
+def csv2dict(fileName):
+    data = numpy.genfromtxt(fileName, delimiter=',', names=True, dtype=None, encoding=None)
+    
+    return {name: data[name] for name in data.dtype.names}
+
 class solution():
 
     def __init__(self, meshFile, solFile):
-
-        self.gamma = 1.4
-        self.Rgas = 287.5
-
+        
         self.mesh = reader(meshFile)
         
         self.x = self.mesh.x
@@ -24,35 +26,9 @@ class solution():
                 
         ff = open(solFile)
 
-        self.r = []
-        self.ru = []
-        self.rv = []
-        self.rE = []
-
-        first = True
-        for row in ff:
-            if first:
-                first = False
-            else:
-                aux = row.split(',')
-                self.r.append(float(aux[0]))
-                self.ru.append(float(aux[1]))
-                self.rv.append(float(aux[2]))
-                self.rE.append(float(aux[3]))
+        self.data = csv2dict(solFile)
 
         ff.close()
-
-        self._toArray()
-        self.calcPMT()
-    
-    def _toArray(self):
-
-        self.r = numpy.array(self.r)
-        self.ru = numpy.array(self.ru)
-        self.rv = numpy.array(self.rv)
-        self.rE = numpy.array(self.rE)
-
-        return None
         
     def elemToTri(self):
     
@@ -66,38 +42,6 @@ class solution():
             
         return None                
         
-    def calcPMT(self):
-       
-        Np = len(self.mesh.p)
-       
-        self.p = numpy.zeros(Np)
-        self.mach = numpy.zeros(Np)
-        self.entro = numpy.zeros(Np)               
-        self.H = numpy.zeros(Np)                
-       
-        for ii in range(0, Np):
-       
-            if(self.con[ii] > 0):
-       
-                u = self.ru[ii]/self.r[ii]
-                v = self.rv[ii]/self.r[ii]
-                E = self.rE[ii]/self.r[ii]
-                
-                RT = (E - (u**2 + v**2)/2)*(self.gamma - 1)
-                
-                self.p[ii] = RT*self.r[ii]
-                
-                c = numpy.sqrt(self.gamma*RT)
-                V = numpy.sqrt(u**2 + v**2)
-                
-                self.mach[ii] = V/c
-
-                self.entro[ii] = self.p[ii]/(self.r[ii]**self.gamma)
-
-                self.H[ii] = E + self.p[ii]/self.r[ii]
-        
-        return None        
-
     def pConnect(self):
     
         self.con = numpy.zeros(len(self.mesh.p))
@@ -133,13 +77,13 @@ if __name__=="__main__":
     
     path = sys.argv[1]
 
-    s = solution(path+"mesh.su2", path+"solution.csv")
+    s = solution(path+"mesh.su2", path+"solution2.csv")
 
     triang = mtri.Triangulation(s.x, s.y, s.elem)
 
     plt.figure()
     plt.title("Static pressure")
-    plt.tricontourf(triang, s.p)
+    plt.tricontourf(triang, s.data['p'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar()  
@@ -147,7 +91,7 @@ if __name__=="__main__":
 
     plt.figure()
     plt.title("Mach")
-    plt.tricontourf(triang, s.mach)
+    plt.tricontourf(triang, s.data['mach'])
     #plt.triplot(triang, 'ko-') 
     plt.axis('equal') 
     plt.colorbar()  
@@ -156,11 +100,11 @@ if __name__=="__main__":
     mar = s.mesh.markers[0]
     mar.getXY(s.mesh)
     
-    inter = mtri.LinearTriInterpolator(triang, s.mach)
+    inter = mtri.LinearTriInterpolator(triang, s.data['mach'])
     mach = inter(mar.x, mar.y)
-    inter = mtri.LinearTriInterpolator(triang, s.p)
+    inter = mtri.LinearTriInterpolator(triang, s.data['p'])
     p = inter(mar.x, mar.y)
-    inter = mtri.LinearTriInterpolator(triang, s.r)
+    inter = mtri.LinearTriInterpolator(triang, s.data['r'])
     r = inter(mar.x, mar.y)
 
 
